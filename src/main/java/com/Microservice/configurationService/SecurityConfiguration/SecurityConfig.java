9603fd70f96961ba921745f4f740d6f7c8940b2e
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2024-02-29 12:38:00 -0300

    .

--- src/main/java/com/Microservice/configurationService/SecurityConfiguration/SecurityConfig.java
+++ src/main/java/com/Microservice/configurationService/SecurityConfiguration/SecurityConfig.java
@@ -3,16 +3,18 @@ package com.Microservice.configurationService.SecurityConfiguration;
 import jakarta.servlet.FilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+/*
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.DefaultSecurityFilterChain;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;*/
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig {
 
+//@EnableWebSecurity
+public class SecurityConfig {
+    /*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(e ->{
@@ -22,5 +24,6 @@ public class SecurityConfig {
         }).httpBasic();
 
         return http.build();
-    }
+    }*/
+
 }
